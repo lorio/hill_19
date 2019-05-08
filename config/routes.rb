781a1592378f8434e392b13535f46b06a3047c66
lorio <lori@loriortiz.com>
2019-05-08 14:50:20 -0400

    Add routes and switches

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   root :to => 'pages#home'
+  
   get 'about' => 'pages#about'
   get 'languages' => 'pages#languages'
   get 'hypnotherapy' => 'pages#hypnotherapy'
   get "writing" => redirect('http://writing.hillarykeel.com')
-  
+  scope :locale do
+    resources :pages
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
