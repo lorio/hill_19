ed29e0933e4d8933fc8dca85bb2bd7e059ea2617
lorio <lori@loriortiz.com>
2019-05-28 12:37:05 -0400

    Add default locale in config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,6 @@ module Hill19
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     # config.i18n.available_locales = [:en, :de]
-    # config.i18n.default_locale = :en
+     config.i18n.default_locale = :en
   end
 end
